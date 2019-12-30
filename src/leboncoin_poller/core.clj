8271003c2fd758d9292497398d4d755700c27110
Peter Onneby <ponneby@xebia.fr>
2019-12-31 00:46:37 +0100

    Cleanup, search on endpoint call

--- src/leboncoin_poller/core.clj
+++ src/leboncoin_poller/core.clj
@@ -8,10 +8,8 @@
 
 (defonce my-cs (clj-http.cookies/cookie-store))
 
-
 (def search-body 
-  "{\"limit\":35,\"limit_alu\":3,\"filters\":{\"category\":{},\"enums\":{\"ad_type\":[\"offer\"]},\"location\":{\"locations\":[]},\"keywords\":{\"text\":\"RFP1000\"},\"ranges\":{}},\"user_id\":\"83373d7d-06a5-4505-bf0f-fec08f928efc\",\"store_id\":\"6348695\"}"
-  )
+  "{\"limit\":35,\"limit_alu\":3,\"filters\":{\"category\":{},\"enums\":{\"ad_type\":[\"offer\"]},\"location\":{\"locations\":[]},\"keywords\":{\"text\":\"RFP1000\"},\"ranges\":{}},\"user_id\":\"83373d7d-06a5-4505-bf0f-fec08f928efc\",\"store_id\":\"6348695\"}")
 
 (def h {
         "Accept" "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"
@@ -27,18 +25,6 @@
         "User-Agent" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
         })
 
-(defn complete []
-  (log/info "Complete for RFPlayer")
-  (:body (http/get "https://api.leboncoin.fr/api/parrot/v1/complete?q=RFP1000" {
-                                                              :headers h
-                                                              :cookie-store        my-cs})))
-(defn location []
-  (log/info "Location for RFPlayer")
-  (:body (http/post "https://api.leboncoin.fr/api/parrot/v3/complete/location" {:body "{\"text\":\"\",\"context\":[]}"
-                                                              :insecure?           true
-                                                              :headers h
-                                                              :content-type :json
-                                                              :cookie-store        my-cs})))
 (defn search []
   (log/info "Search for RFPlayer")
   (:body (http/post "https://api.leboncoin.fr/finder/search" {:body search-body
@@ -51,18 +37,15 @@
   (json/parse-string (search) true))
 
 (defn handler [request]
+  (doseq [ad (:ads (search-results))]
+    (println "Date: " (:first_publication_date ad))
+    (println "Ad: " (:subject ad) " ")
+    )
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+   :body "Checked"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
- (run-jetty handler {:port (Integer/valueOf (or (System/getenv "PORT") "3000")) :join? false}) 
-  (while true 
-    (Thread/sleep 60000)
-    (doseq [ad (:ads (search-results))]
-    (println "Date: " (:first_publication_date ad))
-    (println "Ad: " (:subject ad) " ")
-    ))
-  )
+ (run-jetty handler {:port (Integer/valueOf (or (System/getenv "PORT") "3000")) :join? false}))
